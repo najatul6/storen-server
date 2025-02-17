b2a474f00b1b9adb794c2aa04a3a36bc65e9d033
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-17 20:03:19 +0600

    Update CORS configuration to allow requests from the client application

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const uri = process.env.MONGODB_URI;
 app.use(express.json());
 
 app.use(
-  cors({ origin: ["https://storen-server.vercel.app","http://localhost:5173"] })
+  cors({ origin: ["https://storen-client.vercel.app","http://localhost:5173"] })
 );
 
 
