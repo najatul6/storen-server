da81db67e235604c1f78cf25ee8fe5d0ad64bffc
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-02-17 19:24:11 +0600

    Add endpoint to update a cart by ID

--- index.js
+++ index.js
@@ -205,6 +205,15 @@ async function run() {
       res.send(result);
     });
 
+    app.put("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const update = { $set: req.body };
+      const result = await cartsCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
